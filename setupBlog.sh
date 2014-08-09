3496bc1db20a3c851a8183090d3e1b304c6b77c6
Richard Gill <richard@rgill.co.uk>
2014-08-09 17:58:05 +0100

    Getting the blog setup

---
+++ setupBlog.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+{
+    echo "This script requires superuser access"
+    echo "You will be prompted for your password by sudo."
+
+    # clear any previous sudo permission
+    sudo -k
+
+    # run inside sudo
+    sudo sh <<SCRIPT
+        echo "Building a blog docker instance"
+        docker build -t blog .
+
+        echo "Running container"
+
+
+        echo "You need to run:\n"
+
+        echo "\nThen you can do: \n\n"
+        echo "cd /mount/blog/ && jekyll serve --watch"
+
+SCRIPT
+
+sudo docker run -v $PWD:/mount/blog/ -p 4000:80 -e IP=$(hostname -I | awk '{print $1}') -i -t blog bash
+
+
+}
