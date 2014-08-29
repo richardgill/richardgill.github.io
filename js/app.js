906b3df9a9b8c17e50ca37916bbff115e9984f53
Richard Gill <richard@craft.co>
2014-08-29 12:25:44 +0000

    Made blog posts show actual blog posts in jekyll

--- js/app.js
+++ js/app.js
@@ -64,40 +64,6 @@
     // Get the messages div.
     var formMessages = $('#theme-form-messages');
 
-    // Set up an event listener for the contact form.
-    $(form).submit(function(e) {
-      // Stop the browser from submitting the form.
-      e.preventDefault();
-
-      // Serialize the form data.
-      var formData = $(form).serialize();
-
-      // Submit the form using AJAX.
-      $.ajax({
-        type: 'POST',
-        url: $(form).attr('action'),
-        data: formData
-      })
-      .done(function(response) {
-        // Set the message text.
-        $(formMessages).html(response);
-
-        // Clear the form.
-        $('#contact-name').val('');
-        $('#contact-email').val('');
-        $('#contact-subject').val('');
-        $('#contact-message').val('');
-      })
-      .fail(function(data) {
-        // Set the message text.
-        if (data.responseText !== '') {
-          $(formMessages).html(data.responseText);
-        } else {
-          $(formMessages).html('<div class="alert alert-danger margin-top-40"><button type="button" class="close" data-dismiss="alert">×</button><strong>Error!</strong><br /> Error!</div>');
-        }
-      });
-
-    });
 
   });
   
