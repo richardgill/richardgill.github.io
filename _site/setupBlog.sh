27a739c3e40ca3bf22fafe48c1c410bebc5b7b41
Richard Gill <richard@craft.co>
2014-08-31 20:38:32 +0000

    Revert "fixed master branch to be static html"
    This reverts commit 2caff296ebe43c5f5c66237a60b2cc42e0db3b62.

---
+++ _site/setupBlog.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+{
+    echo "This script requires superuser access"
+    echo "You will be prompted for your password by sudo."
+
+    # clear any previous sudo permission
+    sudo -k
+
+    # run inside sudo
+    sudo sh <<SCRIPT
+        echo "Building a blog docker instance"
+        docker build -t blog .
+
+        echo "Running container"
+
+        echo "\nRunning command: \n\n"
+        echo "cd /mount/blog/ && bundle install && bundle exec jekyll serve --watch --drafts"
+
+SCRIPT
+
+sudo docker run -v $PWD:/mount/blog/ -p 4000:4000 -e IP=$(hostname -I | awk '{print $1}') -i -t blog bash -c "cd /mount/blog/ && bundle install && bundle exec jekyll serve --baseurl '' --drafts && bash"
+
+}
